339f755798ec70fd8d1d8b424d9d580acc6df0c9
Samuel Emeka <samuel.emeka@kibo.school>
2024-01-06 22:44:28 +0100

    mobile search

--- containers/SearchBox.jsx
+++ containers/SearchBox.jsx
@@ -1,15 +1,59 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import styles from '../styles/components/searchbox.module.css'
+import MyLocationIcon from '@mui/icons-material/MyLocation';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBox = () => {
+  const [ optionClass, setOptionClass] = useState(styles.bar);
+  const changeToBuy = () => {
+    setOptionClass(`${styles.bar} ${styles.buy}`)
+  }
+  const changeToRent = () => {
+    setOptionClass(styles.bar);
+  }
   return (
     <div className={styles.container}>
       <div className={styles.searchbox}>
-        <p>This is a searchbox</p>
-        <p>This is a searchbox</p>
-        <p>This is a searchbox</p>
-        <p>This is a searchbox</p>
-        <p>This is a searchbox</p>
+
+        {/* Rent and Buy option slider */}
+
+        <div className={styles.options}>
+          <div className={styles.option}>
+            <p onClick={changeToRent} className={styles.option_text}>Rent</p>
+            <p onClick={changeToBuy} className={styles.option_text}>Buy</p>
+          </div>
+          <div className={styles.active}>
+            <div className={optionClass}></div>
+          </div>
+        </div>
+
+        {/* Input fields */}
+
+        <div className={styles.input_area}>
+          <form>
+          <div className={styles.input_sections}>
+            <div className={styles.input_field}>
+            <label className={styles.labels} htmlFor="location">Location</label><br />
+              <input className={styles.inputs} type="text" id="location" name="location" placeholder='select your city'/><br />
+              <MyLocationIcon className={styles.icon} />
+            </div>
+            <div className={styles.input_field}>
+            <label className={styles.labels} htmlFor="property-type">Property Type</label><br />
+              <input className={styles.inputs} type="text" id="property-type" name="property-type" placeholder='choose property type'/><br />
+              <KeyboardArrowDownIcon className={styles.icon} />
+            </div>
+            <div className={styles.input_field}>
+            <label className={styles.labels} htmlFor="price-range">Price Range</label><br />
+              <input className={styles.inputs} type="text" id="price-range" name="price-range" placeholder='set price range'/><br />
+              <KeyboardArrowDownIcon className={styles.icon} />
+            </div>
+            <button className={styles.submit_button} type='submit'><SearchIcon /></button>
+          </div>
+          </form>
+        </div>
       </div>
     </div>
   )
