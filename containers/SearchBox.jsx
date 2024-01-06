7a40499f161b6e6647269e4f184d3f61fcd2656a
Samuel Emeka <samuel.emeka@kibo.school>
2024-01-06 18:23:00 +0100

    Jan 6 commit Searchbox build

--- containers/SearchBox.jsx
+++ containers/SearchBox.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from '../styles/components/searchbox.module.css'
 
 const SearchBox = () => {
   return (
-    <div></div>
+    <div className={styles.container}>
+      <div className={styles.searchbox}>
+        <p>This is a searchbox</p>
+        <p>This is a searchbox</p>
+        <p>This is a searchbox</p>
+        <p>This is a searchbox</p>
+        <p>This is a searchbox</p>
+      </div>
+    </div>
   )
 }
 
