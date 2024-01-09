1957a5c5d07eed6db453c66cc124ead95fa59b28
Samuel Emeka <samuel.emeka@kibo.school>
2024-01-09 21:57:27 +0100

    Assets update

--- constants/assets.js
+++ constants/assets.js
@@ -13,10 +13,29 @@ export const photoFiles = {
     heroImage: {
         url: 'https://res.cloudinary.com/dnq4kwg8w/image/upload/v1704410352/hero-image.jpg',
         description: 'Find your ideal home today with Luxvilla'
+    },
+    featuredProperties: {
+        img1: {
+        url: 'https://res.cloudinary.com/dnq4kwg8w/image/upload/v1704410351/pexels-maria-orlova-4947782_rmral4.jpg',
+        description:''
+    },
+        img2: {
+        url: 'https://res.cloudinary.com/dnq4kwg8w/image/upload/v1704410350/pexels-maria-orlova-4916516_y8fdon.jpg',
+        description:''
+    },
+        img3: {
+        url: 'https://res.cloudinary.com/dnq4kwg8w/image/upload/v1704410349/pexels-maria-orlova-4946982_n7v7rj.jpg',
+        description:''
+    },
+
+    },
+    callToActionPhoto: {
+        url: 'https://res.cloudinary.com/dnq4kwg8w/image/upload/v1704410350/pexels-alena-darmel-7641899_oevz37.jpg',
+        description: ''
     }
 }
 
-export const feauturePhotos = {
+export const feautureCarouselPhotos = {
     feature1: {
         url: 'https://res.cloudinary.com/dnq4kwg8w/image/upload/v1704744034/features/ko9fkjlpz6xbtklzjayy.svg',
         description: '',
