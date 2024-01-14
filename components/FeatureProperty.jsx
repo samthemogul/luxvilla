f66c1b103382a2f7fffc37a24b6ad0133ee727f4
Samuel Emeka <samuel.emeka@kibo.school>
2024-01-14 22:16:54 +0100

    featured properties dev

---
+++ components/FeatureProperty.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/components/featuredproperties.module.css'
+
+const FeatureProperty = ({image, title, description, location}) => {
+  return (
+    <div>FeatureProperty</div>
+  )
+}
+
+export default FeatureProperty
